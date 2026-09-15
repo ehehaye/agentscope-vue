d75aa34cd6ca2f1797a6ea881b21c6cecc7a852f
ehehaye <hongxin.tang@hotmail.com>
2026-09-15 21:10:05 +0800

    chore: remove unused numeric separator babel plugin
    remove @babel/plugin-transform-numeric-separator from dependencies and config
    since it's no longer needed for the project build

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,4 @@
 module.exports = {
 	presets: ['@vue/cli-plugin-babel/preset'],
-	// webpack 4 的 acorn 不支持数值分隔符（10_000），强制转换
-	plugins: [
-		'@babel/plugin-transform-numeric-separator',
-		// '@babel/plugin-proposal-nullish-coalescing-operator',
-    // '@babel/plugin-proposal-optional-chaining',
-	],
+	plugins: [],
 };
